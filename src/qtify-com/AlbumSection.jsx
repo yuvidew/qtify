675f7dda42311e3bf2351ab5ba894712b072bce4
Yuvi dew <127539111+yuvidew@users.noreply.github.com>
2024-05-02 15:43:10 +0530

    update the file

--- src/qtify-com/AlbumSection.jsx
+++ src/qtify-com/AlbumSection.jsx
@@ -1,44 +1,10 @@
 import { Button } from '@/components/ui/button';
-import React, { useEffect, useRef, useState } from 'react'
+import React, {useState } from 'react'
 import CardComp from './CardComp';
 import Spinner from '@/components/ui/Spinner';
-import { ArrowLeft, ArrowRight } from 'lucide-react';
-import useFetch from '@/hook/useFetch';
 
-const Cards = ({title}) => {
+const Cards = ({albumData , title}) => {
     const [topIsCollapse , setTopIsCollapse] = useState(false)
-    const [topAlbumData , setTopAlbumData] = useState([])
-    const [newAlbumData , setNewAlbumData] = useState([])
-    const [getSongs] = useFetch()
-    const topSliderRef = useRef(null)
-    const newSliderRef = useRef(null)
-
-    const topScrollBtn = (index) => {
-        topSliderRef.current.scrollBy({
-            left: topSliderRef.current.clientWidth * index, // Adjust this based on the slide width
-            behavior: 'smooth',
-        })
-    }
-
-    const newScrollBtn = (index) => {
-        newSliderRef.current.scrollBy({
-            left: newSliderRef.current.clientWidth * index, // Adjust this based on the slide width
-            behavior: 'smooth',
-        })
-    }
-
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const topAlbumData = await  getSongs('https://qtify-backend-labs.crio.do/albums/top')
-            const newAlbumData = await getSongs('https://qtify-backend-labs.crio.do/albums/new')
-
-            setTopAlbumData(topAlbumData)
-            setNewAlbumData(newAlbumData)
-        }
-
-        fetchData()
-    }, [])
 
     return (
         <>
@@ -49,27 +15,8 @@ const Cards = ({title}) => {
                     {!topIsCollapse ? "Show All" : "Collapse"}
                 </Button>
             </div>
-            {topAlbumData !==0 ?(<div className=' relative mt-5'>
-                {!topIsCollapse && <div className=' z-[99] absolute top-[45%] w-full flex items-center justify-between'>
-                    <Button 
-                        variant = "green"  
-                        className = "rounded-[100rem] w-[2.5rem] h-[2.5rem] ml-[-1rem]" 
-                        size = "sm"
-                        onClick={() => topScrollBtn(-1)}
-                    >
-                        <ArrowLeft/>
-                    </Button>
-                    <Button 
-                        variant = "green" 
-                        className = "rounded-[100rem] w-[2.5rem] h-[2.5rem] mr-[-1rem]" 
-                        size = "sm"
-                        onClick={() => topScrollBtn(1)}
-                    >
-                        <ArrowRight className=' h-10 w-10'/>
-                    </Button>
-                </div>}
+            {albumData !==0 ?(<div className=' relative mt-5'>
                 <section 
-                    ref = {topSliderRef}
                     className={ 
                         !topIsCollapse 
                         ? ' flex items-center gap-x-3 overflow-x-scroll scroll-smooth' 
@@ -81,8 +28,8 @@ const Cards = ({title}) => {
                     }}
                 >
                     <div className={!topIsCollapse ? ' flex items-center gap-3 py-2' : "grid grid-cols-6 gap-y-2 gap-x-2"}>
-                        {topAlbumData.length !== 0 ? (
-                            topAlbumData.map(ele => (
+                        {albumData.length !== 0 ? (
+                            albumData.map(ele => (
                                 <CardComp key={ele.id} ele = {ele} />
                             ))
                         ) : (
@@ -94,64 +41,7 @@ const Cards = ({title}) => {
                 </section>
             </div>)
             : (
-                <div>
-                    <Spinner/>
-                </div>
-            )}
-        </section>
-        <section className=' w-[95%] m-auto mb-[2rem]'>
-            <div className=' flex items-center justify-between'>
-                <h1 className=' text-[1.2rem] text-white' >New Album</h1>
-                <Button variant = "ghost" onClick = {() => setTopIsCollapse(!topIsCollapse)}>
-                    {!topIsCollapse ? "Show All" : "Collapse"}
-                </Button>
-            </div>
-            {newAlbumData !==0 ?(<div className=' relative mt-5'>
-                {!topIsCollapse && <div className=' z-[99] absolute top-[45%] w-full flex items-center justify-between'>
-                    <Button 
-                        variant = "green"  
-                        className = "rounded-[100rem] w-[2.5rem] h-[2.5rem] ml-[-1rem]" 
-                        size = "sm"
-                        onClick={() => newScrollBtn(-1)}
-                    >
-                        <ArrowLeft/>
-                    </Button>
-                    <Button 
-                        variant = "green" 
-                        className = "rounded-[100rem] w-[2.5rem] h-[2.5rem] mr-[-1rem]" 
-                        size = "sm"
-                        onClick={() => newScrollBtn(1)}
-                    >
-                        <ArrowRight className=' h-10 w-10'/>
-                    </Button>
-                </div>}
-                <section 
-                    ref = {newSliderRef}
-                    className={ 
-                        !topIsCollapse 
-                        ? ' flex items-center gap-x-3 overflow-x-scroll scroll-smooth' 
-                        : " flex w-full items-center gap-x-3"
-                    }
-
-                    style={{
-                        scrollbarWidth : "none"
-                    }}
-                >
-                    <div className={!topIsCollapse ? ' flex items-center gap-3 py-2' : "grid grid-cols-6 gap-y-2 gap-x-2"}>
-                        {newAlbumData.length !== 0 ? (
-                            newAlbumData.map(ele => (
-                                <CardComp key={ele.id} ele = {ele} />
-                            ))
-                        ) : (
-                            <div className=' flex items-center justify-center w-full h-full'>
-                            <Spinner/>
-                            </div>
-                        )}
-                    </div>
-                </section>
-            </div>)
-            : (
-                <div>
+                <div className=' w-[30rem] m-auto h-[10rem] flex items-center justify-center'>
                     <Spinner/>
                 </div>
             )}
--- src/qtify-com/HomePage.jsx
+++ src/qtify-com/HomePage.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import hero from '../assets/hero.svg'
 import Card from './AlbumSection'
+import useFetch from '@/hook/useFetch'
 
 const HomePage = () => {
+    const [getSongs] = useFetch()
+    const [topAlbumData , setTopAlbumData] = useState([])
+    const [newAlbumData , setNewAlbumData] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const topAlbumData = await getSongs('https://qtify-backend-labs.crio.do/albums/top')
+            const newAlbumData = await getSongs('https://qtify-backend-labs.crio.do/albums/new')
+
+            setTopAlbumData(topAlbumData)
+            setNewAlbumData(newAlbumData)
+        }
+
+        fetchData()
+    }, [])
 
     return (
         <>
@@ -19,7 +35,8 @@ const HomePage = () => {
                 />
             </div>
         </section>
-        <Card  />
+        <Card albumData = {topAlbumData} />
+        <Card albumData = {newAlbumData} />
         </>
     )
 }
