1c5135ca6677c6fcd7ae5e4f17c4ee46ee2a5979
Yuvi dew <127539111+yuvidew@users.noreply.github.com>
2024-05-02 22:12:45 +0530

    update files

--- src/qtify-com/SongCard.jsx
+++ src/qtify-com/SongCard.jsx
@@ -5,22 +5,25 @@ import {
     CardFooter,
     CardHeader,
 } from "@/components/ui/card"
+import { NavLink } from 'react-router-dom'
 
 
 export const SongCard = ({ele}) => {
     return (
-        <Card className = "w-[15rem] h-[16rem] p-0 overflow-hidden group">
-            <CardHeader className = "p-0 h-[85%]">
-                <div className='h-full p-0'>
-                    <img className='h-full object-cover w-full' src={ele.image} alt="" />
-                </div>
-            </CardHeader>
-            <CardContent>
-                <p>Card Content</p>
-            </CardContent>
-            <CardFooter>
-                <p>Card Footer</p>
-            </CardFooter>
-        </Card>
+        <NavLink to={'/'}>
+            <Card className = "w-[15rem] h-[16rem] p-0 overflow-hidden group">
+                <CardHeader className = "p-0 h-[85%]">
+                    <div className='h-full p-0'>
+                        <img className='h-full object-cover w-full' src={ele.image} alt="" />
+                    </div>
+                </CardHeader>
+                <CardFooter>
+                    <p className=' bg-[#121212] mt-2 text-white py-1 px-5 text-sm rounded-xl'>
+                        {ele.likes} Likes
+                    </p>
+                </CardFooter>
+            </Card>
+            <h2 className=' mt-3 text-white'>{ele.title}</h2>
+        </NavLink>
     )
 }
